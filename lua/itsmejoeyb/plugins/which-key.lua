21c62dd3ca9d52e8b23ab18394cc34066a0aaef8
Joey B <joeyb@knorrmarketing.com>
2024-04-05 13:51:15 -0400

    added some plugins

--- lua/itsmejoeyb/plugins/which-key.lua
+++ lua/itsmejoeyb/plugins/which-key.lua
@@ -8,6 +8,6 @@ return {
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    -- refer to the configuration section below 
   },
 }
